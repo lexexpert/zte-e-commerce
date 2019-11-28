ab2822dcc8eb55a364f6e481eba0683d53f6c5a8
Ivan Reshetar <ivan.reshetar@flybits.com>
2019-11-28 14:28:00 -0500

    Update with CartIcon, CartDropdown components and cart reducer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 import {ReactComponent as Logo} from "../../assets/img/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from 'react-redux';
+import CartIcon from "../CartIcon/CartIcon";
+import CartDropdown from "../CartDropdown/CartDropdown";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className={'header'}>
     <Link to={'/'} className={'logo-container'}>
       <Logo className={'logo'}/>
@@ -23,12 +25,17 @@ const Header = ({ currentUser }) => (
           :
           (<Link className={'option'} to={'/signin'}>SIGN IN</Link>)
       }
+      <CartIcon/>
     </div>
+    {
+      hidden ? null : <CartDropdown/>
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,7 +5,6 @@ import {Link} from "react-router-dom";
 
 const HomePage = () => (
   <div className='homepage'>
-    <Link to={'/'}>Home</Link>
     <h1>Welcome to my Homepage</h1>
    <Directory/>
   </div>
