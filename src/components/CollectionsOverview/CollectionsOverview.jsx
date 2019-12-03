069027e44daf9fd7bf60a0996ff1e86545924e6b
Ivan Reshetar <ivan.reshetar@flybits.com>
2019-12-03 14:19:55 -0500

    Update with new Redux flow

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -3,7 +3,7 @@ import './collectionsOverview.scss';
 import CollectionPreview from "../CollectionPreview/CollectionPreview";
 import {createStructuredSelector} from "reselect";
 import {connect} from "react-redux";
-import {selectCollections} from "../../redux/shop/shop.selectors";
+import {selectCollectionsForPreview} from "../../redux/shop/shop.selectors";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
@@ -14,7 +14,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -3,7 +3,7 @@ import './MenuItem.scss';
 import { withRouter } from 'react-router-dom';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}shop/${linkUrl}`)} >
     <div className={'background-image'} style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className='content'>
       <div className='title'>{ title.toUpperCase() }</div>
