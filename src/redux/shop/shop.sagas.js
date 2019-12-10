0a863e23fc0cd22429cfb262c6ca64b2e1279621
Ivan Reshetar <ivan.reshetar@flybits.com>
2019-12-10 16:43:23 -0500

    Update with cart sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import {takeLatest, call, put} from 'redux-saga/effects';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
 import ShopActionsTypes from './shop.types';
 import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 import {fetchCollectionsFail, fetchCollectionsSuccess} from "./shop.actions";
@@ -25,4 +25,8 @@ export function* fetchCollectionsStart() {
     ShopActionsTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   )
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)])
 }
